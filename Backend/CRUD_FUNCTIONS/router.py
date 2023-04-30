e473f1246defd23d5687f6b70710eaae97bd83df
KFGR <figueroa.8254@gmail.com>
2023-04-29 21:59:45 -0400

    .

--- Backend/CRUD_FUNCTIONS/router.py
+++ Backend/CRUD_FUNCTIONS/router.py
@@ -1,8 +1,11 @@
 import sys
 from fastapi import APIRouter, FastAPI, Response
-from Backend.DATABASE.connection import engine
+from sqlalchemy.orm import Session
+# from Backend.CONFIG.connection import engine
+
 user = APIRouter()
 
 @user.get("/")
 def root():
     return {"message":"API is working"}
+
