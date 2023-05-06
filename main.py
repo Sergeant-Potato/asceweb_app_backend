367b2dc68f9e6ae5d40980fb29d83102cf208b5c
KFGR <figueroa.8254@gmail.com>
2023-05-05 20:36:45 -0400

    .

--- main.py
+++ main.py
@@ -11,7 +11,8 @@
 #     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
 
 import os
-from fastapi import Depends, FastAPI, HTTPException, APIRouter, Response
+from fastapi import Depends, FastAPI, HTTPException, APIRouter, Response 
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from pydantic import SecretStr
 from typing import Optional
@@ -22,10 +23,22 @@ from Backend.SCHEMAS import Administrators_Schemas
 from Backend.CONFIG.connection import engine, Base, SessionLocal
 from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED, HTTP_200_OK
 
-# chapter_members.Base.metadata.create_all(bind=connection.engine)
+
 Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+
+"""
+To allow certain origins this is needed at the time of deployment
+origins = [
+    "https://www.inezki.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origings=origins, 
+)"""
+
 # user= APIRouter()
 #dependency
 def get_db():
@@ -61,27 +74,25 @@ def getAdmins(db: Session = Depends(get_db)):
     return dbAdmins
 
 # @app.post("/validation/", response_model=Administrators_Schemas.get_adta)
-@app.post("/validation/", status_code=200 )
-def validateuser(user: Administrators_Schemas.Administrator_LoginAccount_IN, db: Session = Depends(get_db)):
-    # a = ta.validateUser(db, username=admin.userName.get_secret_value(), password=admin.password.get_secret_value())
-    a = ta.validateUser(db, user=user)
-    print(a)
-    if a['msg'] == "User validated":
-        return {"status":HTTP_200_OK, 'message':a}
-    # print(a)
-    # return Response(status_code=HTTP_200_OK)
-    # return {"status":HTTP_200_OK, 'message':a}
-    # return {'userName':a[0], 'password':a[1], 'status': HTTP_200_OK}
+# @app.post("/validation/", status_code=200 )
+# def validateuser(user: Administrators_Schemas.Administrator_LoginAccount_IN, db: Session = Depends(get_db)):
+#     # a = ta.validateUser(db, username=admin.userName.get_secret_value(), password=admin.password.get_secret_value())
+#     a = ta.validateUser(db, user=user)
+#     print(a)
+#     if a['msg'] == "User validated":
+#         return {"status":HTTP_200_OK, 'message':a}
+#     # print(a)
+#     # return Response(status_code=HTTP_200_OK)
+#     # return {"status":HTTP_200_OK, 'message':a}
+#     # return {'userName':a[0], 'password':a[1], 'status': HTTP_200_OK}
 
 @app.get("/ascepupr/user/login/", status_code=HTTP_200_OK, response_model=Administrators_Schemas.Validate_user)
-def validation(username: str, password: SecretStr, token: str, db: Session = Depends(get_db)):
-    print(token)
+def validation(username: str, password: SecretStr, token: str = None, db: Session = Depends(get_db)):
     a = ta.validateUsers(db,username, password, token)
-    print(a)
     if a['status_code'] == 200:
-        return {"status_code":HTTP_200_OK, 'body':a['Token']}
+        return {"status_code":HTTP_200_OK, 'data':a['body']}
     else:
-        return {"status_code":HTTP_401_UNAUTHORIZED, 'body':a['msg']}
+        return {"status_code":HTTP_401_UNAUTHORIZED, 'data':a['body']}
 
 
 
@@ -89,9 +100,4 @@ def validation(username: str, password: SecretStr, token: str, db: Session = Dep
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-    
-
 
